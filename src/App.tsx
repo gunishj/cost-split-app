9405a3c0158237b690f3ab4c0432d35c8745bd64
Gunish Jha <gujha@expediagroup.com>
2025-03-09 18:03:11 +0530

    Initial commit with working react + typescript setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1>Welcome to Cost Split App</h1>
+      <p>Track and split expenses easily with TypeScript & SWC.</p>
+    </div>
+  );
+}
+
+export default App;
